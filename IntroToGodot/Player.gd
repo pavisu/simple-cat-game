19654c98e073f5ddb54941b06972962a2130510e
Pavithra Sureshkumar <146721357+pavisu@users.noreply.github.com>
2023-10-08 14:59:01 +0200

    Add player moves

---
+++ IntroToGodot/Player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var move_speed : float = 200.0
+
+func _physics_process(delta):
+	
+	velocity.x = 0
+	velocity.y = 0
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= 1
+		
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += 1
+		
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 1
+		
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 1
+		
+	velocity *= move_speed
+	
+	# applies the velocity to the player
+	move_and_slide()
+	
